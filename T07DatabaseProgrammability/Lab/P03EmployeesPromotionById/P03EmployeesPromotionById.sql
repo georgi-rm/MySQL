e353a0479d380f1da2bc770504941b9320343b0b
georgi-rm <georgi.rm@gmail.com>
2022-02-12 17:46:03 +0200

    Problem 3 "Employees Promotion By ID" from Lecture: Database Programmability

--- T07DatabaseProgrammability/Lab/P03EmployeesPromotionById/P03EmployeesPromotionById.sql
+++ T07DatabaseProgrammability/Lab/P03EmployeesPromotionById/P03EmployeesPromotionById.sql
@@ -1,11 +1,22 @@
 DELIMITER $$
 CREATE PROCEDURE usp_raise_salary_by_id(id INT)
 BEGIN
-UPDATE `employees` AS e
-SET 
-    e.`salary` = e.`salary` * 1.05
-WHERE
-    e.`employee_id` = id;
+START TRANSACTION;
+IF( (SELECT
+		count(emp.`employee_id`)
+    FROM 
+		`employees` AS emp
+	WHERE
+		emp.`employee_id` = id ) != 1 )
+THEN
+	ROLLBACK;
+ELSE
+	UPDATE `employees` AS e
+	SET 
+		e.`salary` = e.`salary` * 1.05
+	WHERE
+		e.`employee_id` = id;
+END IF;
 END $$
 
 DELIMITER ;
@@ -15,4 +26,4 @@ DROP PROCEDURE usp_raise_salary_by_id;
 SELECT `employee_id`, `salary`
 FROM `employees`;
 
-CALL usp_raise_salary_by_id(1000);
+CALL usp_raise_salary_by_id(17);
