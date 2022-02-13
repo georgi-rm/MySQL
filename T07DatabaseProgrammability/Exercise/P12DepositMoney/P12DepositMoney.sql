06de46a9ca74286b8c00b15700dc207bd90b022c
georgi-rm <georgi.rm@gmail.com>
2022-02-13 22:50:25 +0200

    Problem 12 "Deposit Money" from Exercise: Database Programmability

---
+++ T07DatabaseProgrammability/Exercise/P12DepositMoney/P12DepositMoney.sql
@@ -0,0 +1,24 @@
+DELIMITER $$
+CREATE PROCEDURE usp_deposit_money(`account_id` INT, `money_amount` DECIMAL(19, 4))
+BEGIN
+START TRANSACTION;
+IF(money_amount < 0)
+THEN 
+	ROLLBACK;
+ELSE
+	UPDATE `accounts` AS a
+	SET a.`balance` = a.`balance` + `money_amount`
+	WHERE a.`id` = `account_id`;
+END IF;
+END $$
+
+DELIMITER ;
+
+CALL usp_deposit_money(1, 10);
+
+SELECT 
+    *
+FROM
+    `accounts` AS a
+WHERE
+    a.`id` = 1;
