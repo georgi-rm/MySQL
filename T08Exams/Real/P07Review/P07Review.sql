962252ebe831980eca4e72a5113c14add4a524c6
georgi-rm <georgi.rm@gmail.com>
2022-02-18 22:58:12 +0200

    Problem 7 "Review" from Exam on 13 February 2022

---
+++ T08Exams/Real/P07Review/P07Review.sql
@@ -0,0 +1,7 @@
+SELECT 
+    r.`id`, r.`content`, r.`rating`, r.`picture_url`, r.`published_at`
+FROM
+    `reviews` AS r
+WHERE
+    r.`content` LIKE 'My%' AND char_length(r.`content`) > 61
+ORDER BY r.`rating` DESC;
